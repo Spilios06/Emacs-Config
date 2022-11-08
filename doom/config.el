301a8b3370811fb4832ecf5d31939e7412074ec9
Spilios06 <76039634+Spilios06@users.noreply.github.com>
2022-11-08 20:39:58 +0000

    Chemacs

---
+++ doom/config.el
@@ -0,0 +1,59 @@
+(setq doom-font (font-spec :family "SauceCodePro Nerd Font Mono" :size 15)
+      doom-variable-pitch-font (font-spec :family "Ubuntu" :size 15)
+      doom-big-font (font-spec :family "SauceCodePro Nerd Font Mono" :size 24))
+(after! doom-themes
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t))
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
+(setq display-line-numbers-type t)
+
+(setq doom-theme 'doom-one)
+
+(map! :leader
+      :desc "Org babel tangle" "m B" #'org-babel-tangle)
+(setq org-directory "~/org/")
+(setq org-superstar-headline-bullets-list '("◉" "●" "○" "◆" "●" "○" "◆"))
+      ;;org-superstar-itembullet-alist '((?+ . ?➤) (?- . ?✦)) ; changes +/- symbols in item lists
+
+(defun prefer-horizontal-split ()
+  (set-variable 'split-height-threshold nil t)
+  (set-variable 'split-width-threshold 40 t))
+(add-hook 'markdown-mode-hook 'prefer-horizontal-split)
+(map! :leader
+      :desc "Clone indirect buffer other window" "b c" #'clone-indirect-buffer-other-window)
+
+(use-package dashboard
+  :init
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-startup-banner 'logo)
+  ;(setq dashboard-startup-banner "~/.config/doom/doom-emacs-dash.png")
+  (setq dashboard-center-content t))
+  :config
+  (dashboard-setup-startup-hook)
+(setq doom-fallback-buffer "*dashboard")
+
+(beacon-mode 1)
+
+;;(map! :leader
+;;      (:prefix ("d" . "dired")
+;;       :desc "Open dired" "d" #'dired)
+;;(evil-define-key 'normal dired-mode-map
+;;  (kbd "h") 'dired-up-directory
+;;  (kbd "l") 'dired-open-file
+
+;;(setq dired-open-extensions '(("gif" . "sxiv")
+;;                              ("jpg" . "sxiv")
+;;                              ("png" . "sxiv")
+;;                              ("mkv" . "mpv")
+;;                              ("mp4" . "mpv")))
+
+;;(map! :leader
+;;      (:prefix ("=" . "open file")
+;;       ;;:desc "Edit agenda file" "a" #'(lambda () (interactive) (find-file "~/nc/Org/agenda.org"))
+;;       :desc "Edit doom config.org" "c" #'(lambda () (interactive) (find-file "~/.config/doom/config.org"))
+;;       :desc "Edit doom init.el" "i" #'(lambda () (interactive) (find-file "~/.config/doom/init.el"))
+;;       :desc "Edit doom packages.el" "p" #'(lambda () (interactive) (find-file "~/.config/doom/packages.el"))))
