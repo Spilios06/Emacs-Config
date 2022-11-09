e4dec20e0a7a28888c8973a3285d942297e07adf
Spilios06 <76039634+Spilios06@users.noreply.github.com>
2022-11-09 17:40:05 +0000

    Added Spacemacs

--- emacs-profiles.el
+++ emacs-profiles.el
@@ -1,2 +1,3 @@
 (("default" . ((user-emacs-directory . "~/.config/chemacs/doom")))
- ("gnu" . ((user-emacs-directory . "~/.config/chemacs/gnu"))))
+ ("gnu" . ((user-emacs-directory . "~/.config/chemacs/gnu")))
+ ("spacemacs" . ((user-emacs-directory . "~/.config/chemacs/spacemacs"))))
