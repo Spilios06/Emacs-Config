89173dd95c10709af12545b2f0e2aff884d55af1
Spilios06 <76039634+Spilios06@users.noreply.github.com>
2023-01-12 18:58:45 +0200

    Delete emacs-profiles.el

--- emacs-profiles.el
+++
@@ -1,3 +0,0 @@
-(("default" . ((user-emacs-directory . "~/.config/chemacs/doom")))
- ("gnu" . ((user-emacs-directory . "~/.config/chemacs/gnu")))
- ("spacemacs" . ((user-emacs-directory . "~/.config/chemacs/spacemacs"))))
